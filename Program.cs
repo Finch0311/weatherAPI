06c818143801e8d6487f53535d7c724a54b54a12
Finch0311 <132722383+Finch0311@users.noreply.github.com>
2024-04-29 22:51:36 -0500

    API practice

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Net.Http;
+using weatherAPI;
+
+string key = File.ReadAllText("jsconfig1.json");
+
+string APIKey = JObject.Parse(key).GetValue("APIKey").ToString();
+
+Console.WriteLine("Enter your zip code.");
+var zipCode = Console.ReadLine();
+
+var apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&units=imperial&appid={APIKey}";
+
+Console.WriteLine();
+
+Console.WriteLine($"It is currently {WeatherMap.GetTemp(apiCall)} degrees F.");
+
